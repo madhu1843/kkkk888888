956520cc116fbc6ff1f0cdf1ae06132c5b64cf3d
hp <rnaman51@gmail.com>
2023-02-10 18:17:29 +0530

    Intial commit

---
+++ madhu/src/test/java/madhu/example/MadhuApplicationTests.java
@@ -0,0 +1,13 @@
+package madhu.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
